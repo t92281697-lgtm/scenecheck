720a732543cbcf9cef13758dd913f3313c32b220
t92281697-lgtm <t92281697@gmail.com>
2026-06-28 14:38:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -619,15 +619,15 @@ searchPage.style.display = "block";
     moviesDiv.innerHTML += `
       <div class="card" onclick="showMovie('${movie.id}')">
 
-        <img
-          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-          style="
-          width:100%;
-          height:280px;
-          object-fit:cover;
-          border-radius:10px;
-          margin-bottom:10px;
-         "
+       <img
+        ...
+        style="
+        width:100%;
+        aspect-ratio:2/3;
+        object-fit:cover;
+        border-radius:10px;
+        margin-bottom:10px;
+        "
         >
 
         <h2>${movie.title}</h2>
